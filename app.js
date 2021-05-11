b4eea8aaea16aae253e1db0d02265c64abd7e4ac
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-05-11 17:51:55 +0300

    Refactored app.js

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ let cartBtnsDOM = [];
 class Products {
   async getProducts() {
     try {
-      let data = await fetch('products.json');
-      let dataArray = await data.json();
+      const data = await fetch('products.json');
+      const dataArray = await data.json();
       let products = dataArray.items;
-      products = products.map((item) => {
+      products = products.map(item => {
         const { title, price } = item.fields;
         const { id } = item.sys;
         const { url: img } = item.fields.image.fields.file;
@@ -25,7 +25,7 @@ class Products {
       });
       return products;
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 }
@@ -51,7 +51,7 @@ class LocalStorage {
 class UI {
   showProducts(fetchedProducts) {
     productsContainer.innerHTML = fetchedProducts
-      .map((item) => {
+      .map(item => {
         return `
       <div class="product-card">
           <div class="img-container">
@@ -75,16 +75,16 @@ class UI {
   getCartBtns() {
     const cartBtns = [...document.querySelectorAll('.add-to-cart-btn')];
     cartBtnsDOM = cartBtns;
-    cartBtnsDOM.forEach((btn) => {
+    cartBtnsDOM.forEach(btn => {
       const id = btn.dataset.id;
-      const inCart = cart.find((item) => item.id === id);
+      const inCart = cart.find(item => item.id === id);
       if (inCart) {
         this.disableBtn(btn);
       }
-      btn.addEventListener('click', (e) => {
+      btn.addEventListener('click', e => {
         this.disableBtn(e.target);
         const products = LocalStorage.getProducts();
-        let itemToCart = products.find((item) => item.id === id);
+        let itemToCart = products.find(item => item.id === id);
         itemToCart = { ...itemToCart, amount: 1 };
         cart = [...cart, itemToCart];
         LocalStorage.saveCart(cart);
@@ -101,7 +101,7 @@ class UI {
   }
 
   activateBtn(id) {
-    const buttonToActivate = cartBtnsDOM.find((btn) => btn.dataset.id === id);
+    const buttonToActivate = cartBtnsDOM.find(btn => btn.dataset.id === id);
     buttonToActivate.innerHTML =
       '<i class="fas fa-shopping-cart cart-icon"></i> Add to cart';
     buttonToActivate.disabled = false;
@@ -109,7 +109,7 @@ class UI {
 
   fillCart(cart) {
     cartItemContainer.innerHTML = cart
-      .map((item) => {
+      .map(item => {
         return `<div class="cart-item">
       <img
         src=${item.img}
@@ -178,13 +178,31 @@ class UI {
     cartOverlay.classList.remove('show-cart-overlay');
   }
 
+  checkIfHideCart = () => cart.length === 0 && this.hideCart();
+
+  handleRemoveItem = e => {
+    const id = e.target.dataset.id;
+    this.removeItem(id);
+    const itemToRemoveFromDOM = e.target.parentElement.parentElement;
+    cartItemContainer.removeChild(itemToRemoveFromDOM);
+    this.activateBtn(id);
+    this.checkIfHideCart();
+  };
+
   removeItem(id) {
-    cart = cart.filter((item) => item.id !== id);
+    cart = cart.filter(item => item.id !== id);
     LocalStorage.saveCart(cart);
   }
 
+  handleIncreaseAmount = e => {
+    const id = e.target.dataset.id;
+    this.increaseAmount(id);
+    const increasedItem = cart.find(item => item.id === id);
+    e.target.nextElementSibling.innerText = increasedItem.amount;
+  };
+
   increaseAmount(id) {
-    cart = cart.map((item) => {
+    cart = cart.map(item => {
       if (item.id === id) {
         item.amount = item.amount + 1;
         return item;
@@ -194,8 +212,21 @@ class UI {
     LocalStorage.saveCart(cart);
   }
 
+  handleDecreaseAmount = e => {
+    const id = e.target.dataset.id;
+    this.decreaseAmount(id);
+    const decreasedItem = cart.find(item => item.id === id);
+    e.target.previousElementSibling.innerText = decreasedItem.amount;
+    if (decreasedItem.amount === 0) {
+      this.removeItem(id);
+      this.activateBtn(id);
+      cartItemContainer.removeChild(e.target.parentElement.parentElement);
+      this.checkIfHideCart();
+    }
+  };
+
   decreaseAmount(id) {
-    cart = cart.map((item) => {
+    cart = cart.map(item => {
       if (item.id === id) {
         item.amount = item.amount - 1;
         return item;
@@ -220,47 +251,31 @@ class UI {
       this.hideCart();
     });
 
-    cartItemContainer.addEventListener('click', (e) => {
+    cartItemContainer.addEventListener('click', e => {
       if (e.target.classList.contains('remove-item-btn')) {
-        const id = e.target.dataset.id;
-        this.removeItem(id);
-        this.calcCartValues(cart);
-        const itemToRemoveFromDOM = e.target.parentElement.parentElement;
-        cartItemContainer.removeChild(itemToRemoveFromDOM);
-        this.activateBtn(id);
-        if (cartItemContainer.children.length === 0) {
-          this.hideCart();
-        }
-      } else if (e.target.classList.contains('fa-chevron-up')) {
-        const id = e.target.dataset.id;
-        this.increaseAmount(id);
-        const increasedItem = cart.find((item) => item.id === id);
-        e.target.nextElementSibling.innerText = increasedItem.amount;
-        this.calcCartValues(cart);
-      } else if (e.target.classList.contains('fa-chevron-down')) {
-        const id = e.target.dataset.id;
-        this.decreaseAmount(id);
-        const decreasedItem = cart.find((item) => item.id === id);
-        e.target.previousElementSibling.innerText = decreasedItem.amount;
-        if (decreasedItem.amount === 0) {
-          this.removeItem(id);
-          this.activateBtn(id);
-          cartItemContainer.removeChild(e.target.parentElement.parentElement);
-          if (cartItemContainer.children.length === 0) {
-            this.hideCart();
-          }
-        }
-        this.calcCartValues(cart);
+        this.handleRemoveItem(e);
       }
+
+      if (e.target.classList.contains('fa-chevron-up')) {
+        this.handleIncreaseAmount(e);
+      }
+
+      if (e.target.classList.contains('fa-chevron-down')) {
+        this.handleDecreaseAmount(e);
+      }
+
+      this.calcCartValues(cart);
     });
+
+    cartOverlay.addEventListener('click', this.hideCart);
 
     clearAllItemsBtn.addEventListener('click', () => {
       while (cartItemContainer.children.length > 0) {
         const itemToRemove = cartItemContainer.children[0];
-        const id = itemToRemove.children[1].children[2].dataset.id;
-        this.removeItem(id);
-        this.activateBtn(id);
-        cartItemContainer.removeChild(cartItemContainer.children[0]);
+        const itemToRemoveId = itemToRemove.children[1].children[2].dataset.id;
+        this.removeItem(itemToRemoveId);
+        this.activateBtn(itemToRemoveId);
+        cartItemContainer.removeChild(itemToRemove);
       }
       this.calcCartValues(cart);
       this.hideCart();
@@ -274,7 +289,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   products
     .getProducts()
-    .then((products) => {
+    .then(products => {
       ui.showProducts(products);
       LocalStorage.saveProducts(products);
       ui.setupApp();
